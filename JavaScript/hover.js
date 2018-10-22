28bde1eeb07eefc17e9cc4c6830dbfe1b32898bd
Jameson-Win <jameson.k.thai@gmail.com>
2018-10-22 00:51:46 -0700

    Hovering of Tab
    Hovering over tab works

---
+++ JavaScript/hover.js
@@ -0,0 +1,24 @@
+document.getElementById("tab1").onmouseover = function() { hoverFunction('tab1') }
+document.getElementById("tab2").onmouseover = function() { hoverFunction('tab2') }
+document.getElementById("tab3").onmouseover = function() { hoverFunction('tab3') }
+document.getElementById("tab4").onmouseover = function() { hoverFunction('tab4') }
+
+document.getElementById("tab1").onmouseout = function() { hoverOut('tab1') }
+document.getElementById("tab2").onmouseout = function() { hoverOut('tab2') }
+document.getElementById("tab3").onmouseout = function() { hoverOut('tab3') }
+document.getElementById("tab4").onmouseout = function() { hoverOut('tab4') }
+function hoverFunction(x){
+  var temp = document.getElementById(x);
+  // temp.addEventListener("mouseover", function(event){
+  //    event.target.classList.add("taba");
+  // }, false);
+  event.target.classList.add("taba");
+}
+
+function hoverOut(x){
+  var temp = document.getElementById(x);
+  // temp.addEventListener("mouseout", function(event){
+  //    event.target.classList.remove("taba");
+  // }, false);
+  event.target.classList.remove("taba");
+}
